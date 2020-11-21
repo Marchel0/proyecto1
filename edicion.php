f7377c1d9c9b9365123b41d8d3aa1a7df29eb1f9
Nishior1 <ncereceda@ing.ucsc.cl>
2020-11-21 15:30:46 -0300

    Mejora estetica en la edicion
    Ahora se enlista el edificio con los cuadros de texto y ya no se pueden ingresar vacios a la base de datos

--- edicion.php
+++ edicion.php
@@ -3,12 +3,19 @@
     $nombre=$_POST['nombre_edificio'];
     $aforo=$_POST['aforo_total'];
     $id=$_POST['id_edificio'];
-   
-    $update =   "UPDATE edificio
-                SET nombre_edificio='$nombre' , aforo_total='$aforo'
-                WHERE id_edificio='$id'";
+    if($nombre!=null){
+        $update =   "UPDATE edificio
+        SET nombre_edificio='$nombre'
+        WHERE id_edificio='$id'";
+        $result = mysqli_query($conexion, $update);
+    }
+    if($aforo!=null){
+        $update =   "UPDATE edificio
+        SET aforo_total='$aforo'
+        WHERE id_edificio='$id'";
+        $result = mysqli_query($conexion, $update);
+    }
                 
-    $result = mysqli_query($conexion, $update);
     header("Location: mantenedor.php");
 
 ?>
